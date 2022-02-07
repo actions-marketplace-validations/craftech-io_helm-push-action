13925405637d2ecb5d4707734f7e502c24c4d784
matiri132 <matias@craftech.io>
2022-02-07 13:09:03 -0300

    fix: basic auth error

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 helm plugin install https://github.com/chartmuseum/helm-push.git
 if [ ! -z ${REPO_USERNAME} ] && [ ! -z ${REPO_PASSWORD} ]; then
-  AUTH_OPTIONS="--username=\"${REPO_USERNAME}\" --password=\"${REPO_PASSWORD}\""
+  helm repo add --username="${REPO_USERNAME}" --password="${REPO_PASSWORD}" ${CHART_REPO_NAME} ${CHART_REPO_URL}
+else
+  helm repo add ${CHART_REPO_NAME} ${CHART_REPO_URL}
 fi  
-helm repo add $AUTH_OPTIONS ${CHART_REPO_NAME} ${CHART_REPO_URL}
 cd ${CHART_DIR}
 helm dependency update
 helm cm-push . ${CHART_REPO_NAME}
